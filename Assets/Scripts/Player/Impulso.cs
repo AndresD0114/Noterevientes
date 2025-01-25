a2cf29be1d4b0f9aed763aa47e860a3cde71d0a7
AndresD0114 <andres.gallego0114@gmail.com>
2025-01-25 10:04:16 -0500

    inicio

---
+++ Assets/Scripts/Player/Impulso.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class Impulso : MonoBehaviour
+{
+    public float normalSpeed = 5f;
+    public float rampSpeed = 100f;
+    public float exitImpulseForce = 10000f; // Fuerza del impulso al salir de la rampa
+    private Rigidbody2D rb;
+    private bool onRamp = false;
+    private Vector2 rampExitDirection;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        // Obtener el vector velocidad del rigidbody
+        Vector2 currentVelocity = rb.linearVelocity;
+        Vector2 movement = currentVelocity.normalized;
+
+        if (onRamp)
+        {
+            rb.linearVelocity = movement * rampSpeed;
+        }
+        else
+        {
+            rb.linearVelocity = movement * normalSpeed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ramp"))
+        {
+            print("On ramp");
+            onRamp = true;
+            rampExitDirection = collision.transform.right; // Dirección X del objeto de colisión
+            StopCoroutine("RampExitDelay");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ramp"))
+        {
+            print("Exit ramp");
+            StartCoroutine("RampExitDelay");
+        }
+    }
+
+    IEnumerator RampExitDelay()
+    {   
+        ApplyExitImpulse();
+        yield return new WaitForSeconds(0.2f);// Duración más corta para un impulso más intenso
+        onRamp = false;
+    }
+
+    void ApplyExitImpulse()
+    {
+        Vector2 impulse = new Vector2(rampExitDirection.x * exitImpulseForce, 0);
+        rb.AddForce(impulse, ForceMode2D.Impulse);
+    }
+}
